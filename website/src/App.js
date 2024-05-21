7f824198321616764e5d74d1753ea88303183dc4
TheActualOskar <Oskar.svendborg@gmail.com>
2024-05-21 13:33:49 +0200

    website stuff

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to the Wildlife Camera Project</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a sample React application.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
